feebaaa24892b517a811bb8d75aae197516d6ec4
Arya Wiranata <wiranata.arya.wiranata@gmail.com>
2017-10-18 14:58:44 +0700

    update masuk

--- Admin/masuk.php
+++ Admin/masuk.php
@@ -2,16 +2,19 @@
 <head>
 	<title>Login | Kuncir</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
 <body>
+<!--navigation bar-->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<a class="navbar-brand" href="index.php">KUNCIR</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 	</nav>
+<!--end of navigation bar-->
+<!--form login-->
 	<div class="container">
 		<h1 class="display-4">LOGIN</h1>
 		<form name="form1" method="post" action="login.php">
@@ -33,12 +36,17 @@
 		    <input type="submit" name="update" value="Login" class="btn btn-lg btn-primary">
 		</form>
 	</div>
+<!--end of form login-->
+<!--footer-->
 	<footer class="fixed-bottom bg-dark navbar-dark">
 		<center><h2 class="navbar-brand">2017 | Arsitektur dan Jaringan Komputer</h2></center>
 	</footer>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<!--end of footer-->
+<!--JavaScript-->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+<!--end of JavaScript-->
 </body>
 </html>
 
